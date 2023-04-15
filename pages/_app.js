add03ba00d39ad20bc806fc2dbdcf2eed1efbb79
k1325699 <k1325699@gmail.com>
2023-04-15 22:12:54 +0800

    feat: inital

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,29 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import localFont from "next/font/local";
+const GenJyuuGothic = localFont({
+  src: [
+    {
+      path: "./font/GenJyuuGothic-Normal.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./font/GenJyuuGothic-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./font/GenJyuuGothic-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={GenJyuuGothic.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,58 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontSize: {
+        "h1-d": ["32px", "40px"],
+        "h2-d": ["60px", "76px"],
+        "h3-d": ["44px", "56px"],
+        "h4-d": ["32px", "44px"],
+        "h5-d": ["24px", "30px"],
+        "body-d": ["18px", " 28px"],
+        "height-d": ["24px", "32px"],
+        "h1-m": ["20px", "24px"],
+        "h2-m": ["28px", "36px"],
+        "h3-m": ["24px", "32px"],
+        "h4-m": ["20px", "28px"],
+        "h5-m": ["18px", "28px"],
+        "body-m": ["16px", " 24px"],
+        "height-m": ["18px", "24px"],
+      },
+      letterSpacing: {
+        d: "0.02em",
+        m: "0.05em",
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#007FAB",
+          dark: "#003A4F",
+          tint: "#B0D2DE",
+        },
+        secondary: {
+          DEFAULT: "#FFC37D",
+          dark: "#A46039",
+          tint: "#FFE2A9",
+        },
+        highlight: {
+          DEFAULT: "#FF5136",
+          dark: "#CD331A",
+          tint: "#FFB5A4",
+        },
+        gary: {
+          100: "#000000",
+          80: "#333333",
+          60: "#666666",
+          40: "#999999",
+          20: "#CCCCCC",
+          0: "#FFFFFF",
+        },
       },
     },
   },
   plugins: [],
-}
+};
