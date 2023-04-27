92800da73bb7dc33829c82eb73c9b16157b40e37
k1325699 <k1325699@gmail.com>
2023-04-27 14:36:43 +0800

    fix: 變更title

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,36 @@
-import "@/styles/globals.css";
-import localFont from "next/font/local";
+import '@/styles/globals.css';
+import localFont from 'next/font/local';
+import Head from 'next/head';
 const GenJyuuGothic = localFont({
   src: [
     {
-      path: "./font/GenJyuuGothic-Normal.woff2",
-      weight: "400",
-      style: "normal",
+      path: './font/GenJyuuGothic-Normal.woff2',
+      weight: '400',
+      style: 'normal',
     },
     {
-      path: "./font/GenJyuuGothic-Medium.woff2",
-      weight: "500",
-      style: "normal",
+      path: './font/GenJyuuGothic-Medium.woff2',
+      weight: '500',
+      style: 'normal',
     },
     {
-      path: "./font/GenJyuuGothic-Bold.woff2",
-      weight: "700",
-      style: "normal",
+      path: './font/GenJyuuGothic-Bold.woff2',
+      weight: '700',
+      style: 'normal',
     },
   ],
 });
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={GenJyuuGothic.className}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        {' '}
+        <title>F2E互動式網頁─滾動視差</title>
+      </Head>
+      <main className={GenJyuuGothic.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
